417e4584e1595b80b79f8ca704099a8c419eb7de
Andre Campilan <andreroquecampilan9@gmail.com>
2024-06-19 15:54:17 +0800

    Update UploadPhotoForm.jsx

--- laravel-app/resources/js/Pages/Profile/Partials/UploadPhotoForm.jsx
+++ laravel-app/resources/js/Pages/Profile/Partials/UploadPhotoForm.jsx
@@ -71,6 +71,7 @@ export default function UploadedPhotosForm() {
         <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <h1 className="text-2xl font-bold px-6 py-4">Uploaded Photos</h1> 
                     <div className="p-6 bg-white border-b border-gray-200">
                         <Masonry
                             breakpointCols={breakpointColumnsObj}
